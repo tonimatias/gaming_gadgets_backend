893c3bb3ebf9d355151fe8120ab8826ab625fc2d
tonimatias <tonimatias96@gmail.com>
2022-03-29 10:54:24 +0300

    product table

--- database.sql
+++ database.sql
@@ -12,7 +12,12 @@ create Table category (
 create table product (
 id int PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(255) not null,
-price double (10,2) not null
+price double (10,2) not null,
+image VARCHAR(50),
+category_id int not null,
+index category_id (category_id),
+FOREIGN key (category_id) REFERENCES category(id)
+on delete RESTRICT
 );
 
 insert into category (name) values ('Hiiret');
@@ -22,3 +27,5 @@ insert into category (name) values ('Näppäimistöt');
 insert into category (name) values ('Kuulokkeet');
 
 insert into category (name) values ('Hiirimatot');
+insert into product (name, price, category_id) values ('Hiirimatto2', 120,1);
+insert into product (name, price, category_id) values ('Hiirimatto3', 99,1);
---
+++ products/getproducts.php
@@ -0,0 +1,10 @@
+<?php
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
+
+try {
+    $db = openDb();
+    selectAsJson($db, "select * from category");
+} catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
