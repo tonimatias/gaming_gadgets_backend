b8a9e664ab19fe71679669321b5583c2fa5400b7
tonimatias <tonimatias96@gmail.com>
2022-03-31 13:36:24 +0300

    näppikset kuvia vaille valmis

--- database.sql
+++ database.sql
@@ -37,8 +37,12 @@ insert into product (name, price, description, category_id) values ('Logitech G3
 insert into product (name, price, description, category_id) values ('Razer Basilisk Ultimate + lataustelakka', 189.90, 'Pitkä akunkesto, mukana lataustelakka. Jopa 20 000 DPI! Razer HyperSpeed -langaton teknologia',1);
 insert into product (name, price, description, category_id) values ('Hyper X Pulsefire', 59.90, 'RGB efektit. Suurikokoiset rullat. Yksitoista painiketta, Omron-kytkimet', 1);
 
-
-
+insert into product (name, price, description, category_id) values ('Corsair K63 Compacr', 89.90, 'Cherry MX -kytkimet. Suunniteltu tehopelaamiseen. Lisää tilaa ilman numeronäppäimistöä.', 2);
+insert into product (name, price, description, category_id) values ('Blackstorm RGB Mech 2021 musta', 59.90, 'Bluetooth sekä USB yhteys. Yksilöllisesti ohjelmoitava valaistus.', 2);
+insert into product (name, price, description, category_id) values ('Blackstorm RGB Mech 2021 pinkki', 59.90, 'Bluetooth sekä USB yhteys. Yksilöllisesti ohjelmoitava valaistus.', 2 );
+insert into product (name, price, description, category_id) values ('Hyper X Alloy Origins 60', 119.90, 'Mekaaniset Hyper X Red -kytkimet. Harjattu alumiinirunko. 60 % näppäinasettelu.', 2);
+insert into product (name, price, description, category_id) values ('Blackstorm RGB 2022 Pudding 60% Dual Mode', 69.90, 'Bluetooth 5.0 sekä USB-yhteys. 80 miljoonan painalluksen elinkaari. Pudding Keycaps', 2);
+insert into product (name, price, description, category_id) values ('Corsair K95 Platinum', 179.90, 'Cherry Mx Brown -kytkimet. 8 Mt muisti. Anodisoitu, harjattu alumiinipinta', 2);
 
 insert into product (name, price, image,  category_id) values ('Hiirimatto2',120, "images/hiiri.jpg",4);
 insert into product (name, price, category_id) values ('Hiirimatto3', 99,4);
