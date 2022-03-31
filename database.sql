cd2866a84993fb0bcd8b93a04c894875687531da
tonimatias <tonimatias96@gmail.com>
2022-03-31 12:50:59 +0300

    description on nyt varchar(200)

--- database.sql
+++ database.sql
@@ -14,7 +14,7 @@ id int PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(255) not null,
 price double (10,2) not null,
 image VARCHAR(50),
-description VARCHAR(50),
+description VARCHAR(200),
 category_id int not null,
 index category_id (category_id),
 FOREIGN key (category_id) REFERENCES category(id)
@@ -31,6 +31,7 @@ insert into category (name) values ('Hiirimatot');
 insert into product (name, price, description, category_id) values ('Logitech MX518', 29.90, '16 000 DPI. Klassinen muotoilu kahdeksalla ohjelmoitavalla painikkeella', 1);
 insert into product (name, price, description, category_id) values ('Steelseries Rival 3', 39.90, 'Sopii oikeakätisille pelaajille. 16,8 miljoonan värin RGB valaistus. 60 miljoonan klikkauksen kytkimet', 1);
 insert into product (name, price, description, category_id) values ('Logitech G Pro Wireless', 119.90, 'Kevyt ja kestävä pelihiiri. 16 000 DPI. Langaton LIGHTSPEED',1);
+insert into product (name, price, description, category_id) values ('Logitech G305')
 
 
 
