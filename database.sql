f92806cd326ba589c50958210584cafb172ae4f1
tonimatias <tonimatias96@gmail.com>
2022-03-31 12:46:00 +0300

    kolme ekaa hiirtä laitettu tauluun

--- database.sql
+++ database.sql
@@ -14,6 +14,7 @@ id int PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(255) not null,
 price double (10,2) not null,
 image VARCHAR(50),
+description VARCHAR(50),
 category_id int not null,
 index category_id (category_id),
 FOREIGN key (category_id) REFERENCES category(id)
@@ -27,5 +28,12 @@ insert into category (name) values ('Näppäimistöt');
 insert into category (name) values ('Kuulokkeet');
 
 insert into category (name) values ('Hiirimatot');
+insert into product (name, price, description, category_id) values ('Logitech MX518', 29.90, '16 000 DPI. Klassinen muotoilu kahdeksalla ohjelmoitavalla painikkeella', 1);
+insert into product (name, price, description, category_id) values ('Steelseries Rival 3', 39.90, 'Sopii oikeakätisille pelaajille. 16,8 miljoonan värin RGB valaistus. 60 miljoonan klikkauksen kytkimet', 1);
+insert into product (name, price, description, category_id) values ('Logitech G Pro Wireless', 119.90, 'Kevyt ja kestävä pelihiiri. 16 000 DPI. Langaton LIGHTSPEED',1);
+
+
+
+
 insert into product (name, price, image,  category_id) values ('Hiirimatto2',120, "images/hiiri.jpg",4);
 insert into product (name, price, category_id) values ('Hiirimatto3', 99,4);
