40cb374fd4d0e5fc61a54c8487e28dadac2bad17
SamiNousiainen <n1nosa00@students.oamk.fi>
2022-04-05 14:24:12 +0300

    kuva muutos

--- database.sql
+++ database.sql
@@ -30,7 +30,7 @@ insert into category (name) values ('Kuulokkeet');
 insert into category (name) values ('Hiirimatot');
 
 
-insert into product (name, price, description, category_id) values ('Logitech MX518', 29.90, '16 000 DPI. Klassinen muotoilu kahdeksalla ohjelmoitavalla painikkeella', 1);
+insert into product (name, price, description, category_id, image) values ('Logitech MX518', 29.90, '16 000 DPI. Klassinen muotoilu kahdeksalla ohjelmoitavalla painikkeella', 1, 'logitech_mx518.jpeg');
 insert into product (name, price, description, category_id) values ('Steelseries Rival 3', 39.90, 'Sopii oikeakätisille pelaajille. 16,8 miljoonan värin RGB valaistus. 60 miljoonan klikkauksen kytkimet', 1);
 insert into product (name, price, description, category_id) values ('Logitech G Pro Wireless', 119.90, 'Kevyt ja kestävä pelihiiri. 16 000 DPI. Langaton LIGHTSPEED',1);
 insert into product (name, price, description, category_id) values ('Logitech G305', 54.90, '12 000 DPI. Langaton LIGHTSPEED pelihiiri. Erittäin kevyt.',1);
