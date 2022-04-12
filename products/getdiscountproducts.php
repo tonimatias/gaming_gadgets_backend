5c5f5ca23c75560fd188385235db183e2c397312
JSKarhu <92786584+JSKarhu@users.noreply.github.com>
2022-04-12 10:36:54 +0300

    aletuotteiden tulostus etusivulle korjattu(osittain)

--- products/getdiscountproducts.php
+++ products/getdiscountproducts.php
@@ -8,18 +8,18 @@ $parameters = explode('/', $url);
 
 try {
     $db = openDb();
-    $sql = "select * from product where old_price != null ";
+    $sql = "select * from product where old_price is not null ";
     $query = $db->query($sql);
-    $products = $query->fetch(PDO::FETCH_ASSOC);
+    $products = $query->fetchAll(PDO::FETCH_ASSOC);
 
-   /* $sql = "select * from product where category_id = $category_id";
+    /*$sql = "select * from product where category_id = $category_id";
     $query = $db->query($sql);
-    $products = $query->fetchAll(PDO::FETCH_ASSOC);*/
+    $products = $query->fetch(PDO::FETCH_ASSOC);*/
     
 
     header('HTTP/1.1 200 OK');
     echo json_encode(array(
-     //"category" => $category['name'],
+    // "category" => $category['name'],
      "products" => $products
     ));
 
