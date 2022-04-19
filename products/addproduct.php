4d436757c5dcfb3779ff4e611851a33ebb2089fc
WhiteZett <niklas.haro@gmail.com>
2022-04-19 11:06:57 +0300

    kirjoitusvirheen korjaus

--- products/addproduct.php
+++ products/addproduct.php
@@ -5,13 +5,13 @@ require_once '../inc/headers.php';
 $input = json_decode(file_get_contents('php://input'));
 $name = filter_var($input->name,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $price = filter_var($input->price,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-$category_id = filter_var($input->dategoryid,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$category_id = filter_var($input->categoryid,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
 try {
     $db = openDB();
-    $sql = "instert into product (name,price,image,caterogy_id) values ('$name',$price,'placeholder.png',$category_id)";
+    $sql = "insert into product (name,price,category_id) values ('$name',$price,$category_id)";
     executeInsert($db,$sql);
-    $data = array('id' => $db -> lastInsertId(), 'name' => $name, 'price' => $price, 'image' => 'placeholder.png');
+    $data = array('id' => $db -> lastInsertId(), 'name' => $name, 'price' => $price);
     print json_encode($data);
 } catch (PDOException $pdoex) {
     returnError($pdoex);
