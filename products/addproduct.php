2acb53c855871a5b2e69458e4773db2e418ff006
NiklasHr <niklas.haro@gmail.com>
2022-04-27 14:58:37 +0300

    addproduct koodin korjausta

--- products/addproduct.php
+++ products/addproduct.php
@@ -6,12 +6,13 @@ $input = json_decode(file_get_contents('php://input'));
 $name = filter_var($input->name,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $price = filter_var($input->price,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $category_id = filter_var($input->categoryid,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$description = filter_var($input->description,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
 try {
     $db = openDB();
-    $sql = "insert into product (name,price,category_id) values ('$name',$price,$category_id)";
+    $sql = "insert into product (name,price,description,category_id) values ('$name',$price,'$description',$category_id)";
     executeInsert($db,$sql);
-    $data = array('id' => $db -> lastInsertId(), 'name' => $name, 'price' => $price);
+    $data = array('id' => $db -> lastInsertId(), 'name' => $name, 'price' => $price, 'description' => $description);
     print json_encode($data);
 } catch (PDOException $pdoex) {
     returnError($pdoex);
